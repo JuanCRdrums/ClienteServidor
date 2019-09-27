2f9c342a6e8784b7bfb5ee4917776b086af07614
Juan Camilo Rojas <juankdrums14@gmail.com>
2019-09-27 08:23:31 -0500

    Pruebas de rendimiento.

--- sudoku/fan.py
+++ sudoku/fan.py
@@ -10,15 +10,15 @@ context = zmq.Context()
 board = {}
 
 board["board"] = [
-    [1,0,0,0,0,0,0,9,0],
-    [8,4,0,0,0,2,0,0,0],
-    [0,0,0,3,8,0,2,0,0],
-    [0,0,0,9,0,0,8,5,3],
-    [0,0,0,0,0,0,0,0,0],
-    [5,3,8,0,0,6,0,0,0],
-    [0,0,1,0,7,9,0,0,0],
-    [0,0,0,5,0,0,0,6,7],
-    [0,2,0,0,0,0,0,0,9]
+    [3,5,0,6,0,0,0,0,0],
+    [0,0,4,0,0,0,0,0,0],
+    [0,2,0,0,7,0,8,9,0],
+    [0,0,0,0,0,5,0,0,0],
+    [0,7,0,0,1,0,0,2,0],
+    [0,0,0,4,0,0,0,0,0],
+    [0,3,6,0,9,0,0,1,0],
+    [0,0,0,0,0,0,5,0,0],
+    [0,0,0,0,0,7,0,8,2]
 ]
 
 def find_empty(bo):
@@ -27,6 +27,34 @@ def find_empty(bo):
             if bo["board"][i][j] == 0:
                 return (i, j)  # row, col
     return None
+
+def possibles(bo,pos):
+
+    # Check row
+    candidatos = [1,2,3,4,5,6,7,8,9]
+    for num in candidatos:
+        for i in range(len(bo[0])):
+            if bo[pos[0]][i] == num and pos[1] != i:
+                if num in candidatos:
+                    candidatos.remove(num)
+
+        # Check column
+        for i in range(len(bo)):
+            if bo[i][pos[1]] == num and pos[0] != i:
+                if num in candidatos:
+                    candidatos.remove(num)
+
+        # Check box
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+
+        for i in range(box_y*3, box_y*3 + 3):
+            for j in range(box_x * 3, box_x*3 + 3):
+                if bo[i][j] == num and (i,j) != pos:
+                    if num in candidatos:
+                        candidatos.remove(num)
+
+    return candidatos
 
 
 # socket with workers
@@ -47,7 +75,8 @@ sink.send(b'0')
 
 
 row, col = find_empty(board) #se encuentra la primer celda vacia
+candidatos = possibles(board["board"],(row,col))
 
-for task in range(1,10):
+for task in candidatos:
     board["board"][row][col] = task
     workers.send_json(board)
