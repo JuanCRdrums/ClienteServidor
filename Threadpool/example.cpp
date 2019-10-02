9d797fb1e76ed2ec104ac4a69300d0f8df816857
Juan Camilo Rojas <juankdrums14@gmail.com>
2019-10-02 14:54:19 -0500

    Ejemplo de clase con threadpool

---
+++ Threadpool/example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include "ThreadPool.h"
+
+
+using namespace std;
+
+
+int main()
+{
+
+    ThreadPool pool(thread::hardware_concurrency());
+    vector< future<int> > results;
+
+    for(int i = 0; i < 8; ++i) {
+        results.emplace_back(
+            pool.enqueue([i] {
+                cout << "hello " << i << endl;
+                this_thread::sleep_for(chrono::seconds(1));
+                cout << "world " << i << endl;
+                return i*i;
+            })
+        );
+    }
+
+    for(auto && result: results)
+        cout << result.get() << ' ';
+    cout << endl;
+
+    return 0;
+}
